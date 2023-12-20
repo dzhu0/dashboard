ab237a56c2007529b30deabc3afa54524342e945
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-19 18:30:18 -0600

    Major Refactor
    Old quote API does not work, therefore a new API is used. Created functions that will load the background image, weather, and quote. Other small refactors and relocate code in the file for better readability.

--- index.js
+++ index.js
@@ -1,80 +1,86 @@
 const URL_UNSPLASH_API = "https://apis.scrimba.com/unsplash/photos/random"
 const URL_WEATHER_API = "https://apis.scrimba.com/openweathermap/data/2.5/weather"
-const URL_QUOTES_API = "https://api.goprogram.ai/inspiration"
+const URL_QUOTES_API = "https://api.api-ninjas.com/v1/quotes?category=inspirational"
 
-const author = document.getElementById('author')
 const weather = document.getElementById("weather")
+const time = document.getElementById("time")
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
 
 const user = { name: "Daniel" }
+
+setTimeout(() => {
+    window.location.reload()
+}, 1800000)
+
+setInterval(getTimeHtml, 1000)
 
 fetch(`${URL_UNSPLASH_API}?orientation=landscape&query=nature`)
     .then(res => {
         if (!res.ok) throw Error("Unsplash photo not available!")
-
         return res.json()
     })
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
-        author.textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        console.error(err)
+    .then(loadBackground)
+    .catch(error => {
+        console.error(error)
         fetch("./defaultBackground.json")
             .then(res => res.json())
-            .then(data => {
-                document.body.style.backgroundImage = `url(${data.urls.full})`
-                author.textContent = `By: ${data.user.name}`
-            })
+            .then(loadBackground)
+    })
+
+fetch(URL_QUOTES_API, { headers: { "X-Api-Key": "7l3ESX18G2BdeSM/9F1+TQ==NC7YNEQxAkCOA8Ay" } })
+    .then(res => {
+        if (!res.ok) throw Error("Quote not available!")
+        return res.json()
+    })
+    .then(loadQuote)
+    .catch(err => {
+        console.error(err)
     })
 
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`${URL_WEATHER_API}?units=metric&lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
         .then(res => {
             if (!res.ok) throw Error("Weather data not available!")
-
             return res.json()
         })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            weather.innerHTML = `
-                <div class="weather-top">
-                    <img src=${iconUrl} class="weather-icon"/>
-                    <p class="weather-temp">${Math.round(data.main.temp)}&#8451</p>
-                </div>
-                <p class="weather-city">${data.name}</p>`
-        })
+        .then(loadWeather)
         .catch(err => {
             console.error(err)
-            weather.innerHTML = `<p class="weather-city">No data</p>`
         })
 })
 
-fetch(URL_QUOTES_API)
-    .then(res => {
-        if (!res.ok) throw Error("Quote not available!")
-
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById('quote').textContent = `
-            "${data.quote}" - ${data.author}`
-    })
-
 function getTimeHtml() {
     const date = new Date()
-    document.getElementById('time').innerHTML = `
+    time.innerHTML = `
         <h1>${date.toLocaleTimeString("en-us", { timeStyle: "short" })}</h1>
         <p>${getGreeting(date.getHours())}</p>`
 }
 
 function getGreeting(hour) {
-    let greeting =""
+    let partOfDay = "evening"
 
-    if (hour < 12) greeting = "Good morning"
-    else if (hour < 18) greeting = "Good afternoon"
-    else greeting = "Good evening"
+    if (hour < 12) partOfDay = "morning"
+    else if (hour < 18) partOfDay = "afternoon"
 
-    return `${greeting}, ${user.name}.`
+    return `Good ${partOfDay}, ${user.name}.`
 }
 
-setInterval(getTimeHtml, 1000)
+function loadBackground(data) {
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    author.textContent = `By: ${data.user.name}`
+}
+
+function loadWeather(data) {
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    weather.innerHTML = `
+        <div class="weather-top">
+            <img src=${iconUrl} class="weather-icon">
+            <p class="weather-temp">${Math.round(data.main.temp)}&#8451</p>
+        </div>
+        <p class="weather-city">${data.name}</p>`
+}
+
+function loadQuote(data) {
+    quote.textContent = `"${data[0].quote}" - ${data[0].author}`
+}
