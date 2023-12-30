fa94663a4ab9d23fc67d2ebae8621b941788dffb
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-30 16:11:51 -0600

    Add Comments
    Clear and concise comments have been incorporated to improve maintainability and readability.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// Define constants for API endpoints
 const URL_UNSPLASH_API = "https://apis.scrimba.com/unsplash/photos/random"
 const URL_WEATHER_API = "https://apis.scrimba.com/openweathermap/data/2.5/weather"
 const URL_QUOTES_API = "https://api.api-ninjas.com/v1/quotes?category=inspirational"
 
+// DOM elements for displaying weather, time, quote, and author
 const weather = document.getElementById("weather")
 const time = document.getElementById("time")
 const quote = document.getElementById("quote")
 const author = document.getElementById("author")
 
+// User object with a name property
 const user = { name: "Daniel" }
 
+// Reload the page every 30 minutes (1800000 milliseconds)
 setTimeout(() => {
     window.location.reload()
 }, 1800000)
 
+// Update the time display every second
 setInterval(getTimeHtml, 1000)
 
+// Fetch a random nature photo from Unsplash API for the background
 fetch(`${URL_UNSPLASH_API}?orientation=landscape&query=nature`)
     .then(res => {
         if (!res.ok) throw Error("Unsplash photo not available!")
@@ -22,12 +28,14 @@ fetch(`${URL_UNSPLASH_API}?orientation=landscape&query=nature`)
     })
     .then(loadBackground)
     .catch(error => {
+        // Handle errors and load a default background if necessary
         console.error(error)
         fetch("./defaultBackground.json")
             .then(res => res.json())
             .then(loadBackground)
     })
 
+// Fetch an inspirational quote from Quotes API
 fetch(URL_QUOTES_API, { headers: { "X-Api-Key": "7l3ESX18G2BdeSM/9F1+TQ==NC7YNEQxAkCOA8Ay" } })
     .then(res => {
         if (!res.ok) throw Error("Quote not available!")
@@ -35,9 +43,11 @@ fetch(URL_QUOTES_API, { headers: { "X-Api-Key": "7l3ESX18G2BdeSM/9F1+TQ==NC7YNEQ
     })
     .then(loadQuote)
     .catch(err => {
+        // Handle errors related to fetching quotes
         console.error(err)
     })
 
+// Get the user's current geolocation and fetch weather data based on it
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`${URL_WEATHER_API}?units=metric&lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
         .then(res => {
@@ -46,10 +56,12 @@ navigator.geolocation.getCurrentPosition(position => {
         })
         .then(loadWeather)
         .catch(err => {
+            // Handle errors related to fetching weather data
             console.error(err)
         })
 })
 
+// Function to update the time display
 function getTimeHtml() {
     const date = new Date()
     time.innerHTML = `
@@ -57,6 +69,7 @@ function getTimeHtml() {
         <p>${getGreeting(date.getHours())}</p>`
 }
 
+// Function to determine the greeting based on the current hour
 function getGreeting(hour) {
     let partOfDay = "evening"
 
@@ -66,11 +79,13 @@ function getGreeting(hour) {
     return `Good ${partOfDay}, ${user.name}.`
 }
 
+// Function to load the background image
 function loadBackground(data) {
     document.body.style.backgroundImage = `url(${data.urls.full})`
     author.textContent = `By: ${data.user.name}`
 }
 
+// Function to load and display weather information
 function loadWeather(data) {
     const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     weather.innerHTML = `
@@ -81,6 +96,7 @@ function loadWeather(data) {
         <p class="weather-city">${data.name}</p>`
 }
 
+// Function to load and display an inspirational quote
 function loadQuote(data) {
     quote.textContent = `"${data[0].quote}" - ${data[0].author}`
 }
